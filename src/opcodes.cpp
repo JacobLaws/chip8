b7a5e5ce3fc6799d39e723aa60d00eac83a15caf
JacobLaws <laws.v.jacob@gmail.com>
2020-08-21 19:19:15 -0700

    Added opcodes.hpp and opcodes.cpp

---
+++ src/opcodes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LawsV on 8/21/2020.
+//
+
+#include "opcodes.hpp"
---
+++ src/opcodes.hpp
@@ -0,0 +1,8 @@
+//
+// Created by LawsV on 8/21/2020.
+//
+
+#ifndef CHIP8_OPCODES_H
+#define CHIP8_OPCODES_H
+
+#endif //CHIP8_OPCODES_H
